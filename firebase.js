a9b843664531343dc4ac1126fbcb00aab43d1eb9
Gabbie Bade <53550145+gbads@users.noreply.github.com>
2023-11-18 14:40:32 -0800

    exported db

--- firebase.js
+++ firebase.js
@@ -17,4 +17,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+export const db = getFirestore(app);
